91cbedeafe01f2847ad27517ed3dcbb8c323ab2e
BipinKoirala7 <bipinkoirala2061@gmail.com>
2026-06-10 17:01:28 +0545

    deployment

--- backend/app.ts
+++ backend/app.ts
@@ -12,7 +12,12 @@ import profileRoutes from "./routes/profile.routes";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 const mongoUrl = process.env.MONGODB_URL || "";
 mongoose
